4c6d9e98fa758692c6f90d83cf712d8361a4e874
SeoMinJae <hey_minj@naver.com>
2024-01-30 12:20:43 +0900

    회원가입 수정

--- signup/urls.py
+++ signup/urls.py
@@ -8,6 +8,4 @@ from . import views
 urlpatterns = [
     path('test/', views.index),
     path('', CreateUserAPIView.as_view(), name='signup'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
