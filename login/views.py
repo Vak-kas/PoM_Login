824ea84e3559ed096910de318a89753dcd011ae2
SeoMinJae <hey_minj@naver.com>
2024-01-30 10:10:14 +0900

    APIView사용 회원가입 함수 작성

--- login/views.py
+++ login/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
+
 
 # Create your views here.
-from django.http import HttpResponse
+class CreateUserAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 def index(request):
