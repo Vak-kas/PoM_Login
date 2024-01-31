46722f34b8536e5d3be53b614c172daf5bccc42b
서민재 <hey_minj@naver.com>
2024-01-31 20:01:40 +0900

    로그인 기능 세팅

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     'signup',
+    'login',
 ]
 
 REST_FRAMEWORK = {
--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("signup/", include('signup.urls')),
+    path('login/', views.login_view, name='login'),
 ]
