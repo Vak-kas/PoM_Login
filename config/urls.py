804dc6bb4dab140dc0312af1fa1d17321b4dc540
서민재 <hey_minj@naver.com>
2024-02-10 14:14:11 +0900

    jwt 토큰 수정

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from login.views import CustomTokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("signup/", include('signup.urls')),
     path('login/', include('login.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
