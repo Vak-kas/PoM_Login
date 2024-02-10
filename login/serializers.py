21bbc3ea84b79c6e5999a509e5246d034f4fb5e9
서민재 <hey_minj@naver.com>
2024-02-10 20:35:22 +0900

    jwt 정보 등록

--- login/serializers.py
+++ login/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 
@@ -7,7 +10,7 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
 
-        # 사용자 정보를 토큰에 추가
+        # 사용자의 기본 정보 추가
         token['id'] = user.id
         token['name'] = user.name
         token['email'] = user.email
@@ -16,20 +19,26 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         # 사용자 유형에 따른 추가 정보 포함
         if user.user_type == 'e':
-            enterprise = user.enterprise_set.first()
-            if enterprise:
+            try:
+                enterprise = user.enterprise  # '_set' 접미사 제거
                 token['enterprise'] = {
                     'company_name': enterprise.company_name,
                     'company_code': enterprise.company_code,
                     'department': enterprise.department,
                     'position': enterprise.position
                 }
+            except User.enterprise.RelatedObjectDoesNotExist:
+                token['enterprise'] = None
+
         elif user.user_type == 'i':
-            individual = user.individual_set.first()
-            if individual:
+            try:
+                individual = user.individual  # '_set' 접미사 제거
                 token['individual'] = {
                     'school': individual.school,
                     'department': individual.department
                 }
+            except User.individual.RelatedObjectDoesNotExist:
+                token['individual'] = None
 
         return token
+
--- login/views.py
+++ login/views.py
@@ -1,65 +1,6 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import authenticate
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import CustomTokenObtainPairSerializer
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
-
-class LoginView(APIView):
-    def post(self, request):
-        email = request.data.get('email')
-        password = request.data.get('password')
-
-        user = authenticate(request, email=email, password=password)
-
-        if user is not None:
-            refresh = RefreshToken.for_user(user)
-            access_token = str(refresh.access_token)
-
-            # 사용자 정보를 JSON 형식으로 구성
-            user_info = {
-                'email': user.email,
-                'nickname': user.nickname,
-                # 추가 필요한 사용자 정보들을 여기에 포함
-            }
-
-            # 응답 구성
-            response_data = {
-                'access_token': access_token,
-                'user_info': user_info,
-                'message': '환영합니다!'
-            }
-
-            return Response(response_data, status=status.HTTP_200_OK)
-        else:
-            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])  # 로그인이 필요한 엔드포인트임을 지정
-def user_info(request):
-    user = request.user
-
-    # 사용자 정보 구성 (필요한 필드 추가 가능)
-    user_info = {
-        'email': user.email,
-        'nickname': user.nickname,
-        'id' : user.id,
-        # 필요한 추가 정보 포함
-    }
-
-    # 응답 데이터 구성
-    response_data = {
-        'access_token': '',  # 토큰은 이미 로그인 시에 발급되었으므로 빈 문자열
-        'user_info': user_info,
-        'message': f"{user.nickname} 환영합니다!"
-    }
-
-    return Response(response_data, status=status.HTTP_200_OK)
